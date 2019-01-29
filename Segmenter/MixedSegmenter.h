afe8784b4c2b7bd786b35b81dd86657a5f26957e
conley <743703241@qq.com>
2019-01-29 16:07:32 +0800

    [update] 新增混合分词器

---
+++ Segmenter/MixedSegmenter.h
@@ -0,0 +1,61 @@
+#ifndef KLP_MIXEDSEGMENTER_H
+#define KLP_MIXEDSEGMENTER_H
+#include "DAGSegmenter.h"
+#include "HMMSegmenter.h"
+#include <boost/algorithm/string.hpp>
+
+template<class DIC_SEGMENTER, class LABEL_SEGMENTER>
+class MixedSegmenter
+{
+public:
+    MixedSegmenter(DIC_SEGMENTER &dic_segmenter, LABEL_SEGMENTER &label_segmenter):
+        _dic_segmenter(dic_segmenter),
+        _label_segmenter(label_segmenter)
+    {
+
+    }
+
+    MixedSegmenter(MixedSegmenter const&) = delete;
+    MixedSegmenter const& operator=(MixedSegmenter const&) = delete;
+
+    void segment(std::wstring const& sentence, std::vector<std::wstring> &words)
+    {
+        std::vector<std::wstring> dag_words;
+        _dic_segmenter.segment(sentence, dag_words);
+
+        std::vector<std::wstring> single_words;
+        for (auto pos = dag_words.begin(); pos != dag_words.end(); ++pos)
+        {
+            if(pos->length() == 1){
+                single_words.push_back(std::move(*pos));
+            }else if (pos->length() > 1 && !single_words.empty()){
+                std::wstring s = boost::join(single_words, "");
+                single_words.clear();
+                _label_segmenter.segment(s, single_words);
+                for(auto & w: single_words){
+                    words.push_back(std::move(w));
+                }
+                words.push_back(std::move(*pos));
+                single_words.clear();
+            }else{
+                words.push_back(std::move(*pos));
+            }
+        }
+
+        if (single_words.empty()){
+            return;
+        }
+
+        std::wstring s = boost::join(single_words, "");
+        single_words.clear();
+        _label_segmenter.segment(s, single_words);
+        for(auto & w: single_words){
+            words.push_back(std::move(w));
+        }
+    }
+
+private:
+    DIC_SEGMENTER& _dic_segmenter;
+    LABEL_SEGMENTER& _label_segmenter;
+};
+#endif
--- main.cpp
+++ main.cpp
@@ -4,9 +4,10 @@
 #include "Segmenter/SentenceDAG.h"
 #include "Segmenter/DAGSegmenter.h"
 #include "Segmenter/HMMSegmenter.h"
+#include "Segmenter/MixedSegmenter.h"
 
 int main(int argc, const char* argv[]) {
-    if (argc != 3){
+    if (argc != 4){
         std::cout << "Usage: " << argv[0] << " <corpus> <sentence>" << std::endl;
         return -1;
     }
@@ -17,13 +18,35 @@ int main(int argc, const char* argv[]) {
 
     HMMSegmenter hmm{};
     hmm.fit(argv[1]);
-    hmm.segment(codec.from_bytes(argv[2]), words);
+    hmm.segment(codec.from_bytes(argv[3]), words);
 
-
+    std::cout << "hmm segment: ";
     for (auto const& w: words){
         std::cout << codec.to_bytes(w) << " ";
     }
     std::cout << std::endl;
 
+    auto unigram_lm = UnigramLanguageModel{};
+    unigram_lm.fit(argv[2]);
+    auto dag_segmenter = DAGSegmenter<UnigramLanguageModel>(unigram_lm);
+    
+    words.clear();
+    dag_segmenter.segment(codec.from_bytes(argv[3]), words);
+
+     std::cout << "dag segment: ";
+    for (auto const& w: words){
+        std::cout << codec.to_bytes(w) << " ";
+    }
+    std::cout << std::endl;   
+
+    MixedSegmenter<DAGSegmenter<UnigramLanguageModel>, HMMSegmenter> mixed_segmenter(dag_segmenter, hmm);
+    words.clear();
+
+    mixed_segmenter.segment(codec.from_bytes(argv[3]), words);
+    std::cout << "mixed segment: ";
+    for (auto const& w: words){
+        std::cout << codec.to_bytes(w) << " ";
+    }
+    std::cout << std::endl;      
     return 0;
 }
