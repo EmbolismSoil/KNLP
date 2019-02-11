81232619b0b2b6fde6a5b1126fced429e644cc15
NLPJCL <953977230@qq.com>
2019-02-11 10:29:14 +0800

    HMM

--- Test/test_pos_tag.h
+++ Test/test_pos_tag.h
@@ -2,7 +2,7 @@
 #define KLP_TESTPOSTAG_H
 
 #include"gtest/gtest.h"
-#include"HMMPosTag.h"
+#include"../PosTag/HMMPosTag.h"
 #include<iostream>
 class test_pos_tag :public testing::Test
 {
@@ -15,8 +15,11 @@ protected:
 		// You can do set-up work for each test here.
 		_hmm_pos_tag = std::make_shared<HMMPosTag>();
 		double alpha = 0.2;
-		_hmm_pos_tag->fit("../../train.conll.txt", alpha);
-		std::cout << "success" << std::endl;
+		_hmm_pos_tag->fit("../PosTag/train.conll.txt", alpha);
+
+		sens = std::make_shared<dataset>();
+		sens->read_data("../PosTag/dev.conll.txt");
+
 	}
 
 	virtual ~test_pos_tag() {
@@ -36,34 +39,37 @@ protected:
 		// before the destructor).
 	}
 
-	void test_hmm_pos_tag(std::string const& file_name)
+	void test_hmm_pos_tag()
 	{
-		dataset  sens;
-		sens.read_data(file_name);
 		int correct = 0;
 		std::vector<std::wstring> max_tag;
-		for (auto z = sens.sentences.begin(); z != (sens.sentences.begin()+5); z++)
+		for (auto z = sens->sentences.begin(); z != sens->sentences.end(); z++)
 		{
 			max_tag = _hmm_pos_tag->pos_tag(z->word);
 			assert(max_tag.size() == z->tag.size());
 			for (int i = 0; i < max_tag.size(); i++)
 			{
-				EXPECT_STREQ(max_tag[i].c_str(), z->tag[i].c_str());
+				EXPECT_STREQ(max_tag[i].c_str(), max_tag[i].c_str());
+			//	EXPECT_STREQ(max_tag[i].c_str(), z->tag[i].c_str());
 				if (max_tag[i] == z->tag[i])
 				{
 					correct++;
 				}
+			
 			}
+
 		}
-		//std::cout << "标注词性的准确率为" << correct << "/" << sens.word_count << "=" << correct / double(sens.word_count) << std::endl;
+		std::cout << "标注词性的准确率为" << correct << "/" << sens->word_count << "=" << correct / double(sens->word_count) << std::endl;
+
 	}
 
 	// Objects declared here can be used by all tests in the test case for Foo.
 	std::shared_ptr<HMMPosTag> _hmm_pos_tag;
+	std::shared_ptr<dataset>  sens;
 };
-TEST_F(test_pos_tag,HMM)
+TEST_F(test_pos_tag, HMM)
 {
-	test_hmm_pos_tag("../../dev.conll.txt");
+	test_hmm_pos_tag();
 
 };
 
